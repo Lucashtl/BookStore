61814b211ef43a51c59c8c62b02fc9f446ba5a3e
Lucas Fernandes <lucasuzumaki69@hotmail.com>
2025-04-14 19:56:41 -0300

    exercicio autentificação

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -2,9 +2,15 @@ from rest_framework.viewsets import ModelViewSet
 
 from order.models import Order
 from order.serializers import OrderSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
 
 class OrderViewSet(ModelViewSet):
-
+    authentication_class = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+    queryset = Order.objects.all().order_by('id')
+    
+    class Meta:
+        ordering = ['-id']

3bfa3be5afafc4c5c452a1c6208a229f3226a00f
Lucas Fernandes <lucasuzumaki69@hotmail.com>
2025-04-16 19:33:54 -0300

    subindo projeto final

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l=a)1403ji*xs9_t%%m@(5c&5(#lp&opn63u)%(s@%nef+#6f+"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
@@ -159,4 +159,4 @@ INTERNAL_IPS = [
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']

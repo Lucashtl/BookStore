3cd43960df5ece947eeda783dc307069157a7a15
Lucas Fernandes <lucasuzumaki69@hotmail.com>
2025-04-16 21:30:11 -0300

    render

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -157,8 +157,5 @@ INTERNAL_IPS = [
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = [
-    'bookstore-9amb.onrender.com',
-    'localhost',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['bookstore-c8hf.onrender.com', 'localhost', '127.0.0.1']
+
